415a424e855f186a399cecefd33589e80f2d1118
Patrick Baumgartner <contact@patbaumgartner.com>
2024-12-31 14:08:38 +0100

    Initial project structure

---
+++ src/main/java/com/patbumgartner/swiss/coupon/booster/SwissCouponBoosterApplication.java
@@ -0,0 +1,13 @@
+package com.patbumgartner.swiss.coupon.booster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwissCouponBoosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwissCouponBoosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patbumgartner/swiss/coupon/booster/SwissCouponBoosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patbumgartner.swiss.coupon.booster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwissCouponBoosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
